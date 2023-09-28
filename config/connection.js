08af1ea26a77853b6dc060be70d99cfb9f7c2aff
newprice247 <newprice247@gmail.com>
2023-09-28 10:39:11 -0400

    add sequilizer

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,9 @@
-require('dotenv').config();
-
 const Sequelize = require('sequelize');
 
-const sequelize = process.env.JAWSDB_URL
-  ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-      host: 'localhost',
-      dialect: 'mysql',
-      dialectOptions: {
-        decimalNumbers: true,
-      },
-    });
+const URI = process.env.MYSQL_URI
+
+const sequelize = new Sequelize(URI);
+
+
 
 module.exports = sequelize;
--- server.js
+++ server.js
@@ -1,7 +1,9 @@
-const express = require('express');
-const sequelize = require('./config/connection');
+require('dotenv').config();
 
+const express = require('express');
 const routes = require('./routes');
+// import sequelize connection
+const sequelize = require('./config/connection');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -11,8 +13,6 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(routes);
 
-sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => {
-    console.log(`App listening on port ${PORT}!`);
-  });
-});
+// sync sequelize models to the database, then turn on the server
+sequelize.sync()
+    .then( () => app.listen(PORT, () => console.log(`Now listening on port ${PORT}`)) );
