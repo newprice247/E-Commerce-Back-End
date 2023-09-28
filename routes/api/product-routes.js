273d6673b6f8be5201c054e1e304ea6ac0b437d8
newprice247 <newprice247@gmail.com>
2023-09-28 13:40:41 -0400

    add readme

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -106,10 +106,10 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
-  Product.delete({
+  Product.destroy({
     where: { id: req.params.id }
   })
-  .then((product) => res.json(product))
+  .then(() => res.json({ message: 'Product deleted' }))
   .catch((err) => { res.json(err) });
 });
 
